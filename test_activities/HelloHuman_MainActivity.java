ca1dc7c8e7ba992021524b37bb537454e431f86e
Luca Iocchi <iocchi@diag.uniroma1.it>
2020-04-22 21:58:10 +0200

    HelloHuman activity

---
+++ test_activities/HelloHuman_MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.hellopepper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
+public class MainActivity  extends RobotActivity implements RobotLifecycleCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Register the RobotLifecycleCallbacks to this Activity.
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        // Create a new say action.
+        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("Hello human!") // Set the text to say.
+                .build(); // Build the say action.
+
+        // Execute the action.
+        say.run();
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // Nothing here.
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // The robot focus is refused.
+    }
+}
